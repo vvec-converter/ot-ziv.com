dcc8ea9a5b29d9d9ae4bb17b71be1da847e91dc5
vvc-converter <221469612+vvec-converter@users.noreply.github.com>
2026-08-05 20:18:57 +0300

    Update sw-simple.js

--- sw-simple.js
+++ sw-simple.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function(event) {
         caches.open(CACHE_NAME)
             .then(function(cache) {
                 return cache.addAll([
-                    'https://sites.google.com/view/ot-ziv/%D0%BD%D0%B0%D0%BF%D0%B8%D1%81%D0%B0%D1%82%D1%8C-%D0%BE%D1%82%D0%B7%D1%8B%D0%B2'
+                    'https://www.google.com/url?q=https://ot-ziv.com/...'
                 ]);
             })
             .then(function() {
