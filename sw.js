89f1e7f4d96b7cf7f1832b0d11b34c3c225b1f9f
vvc-converter <221469612+vvec-converter@users.noreply.github.com>
2026-08-05 20:18:18 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = "casino-vodka-cache-v3";
 const CORE_ASSETS = [
-  "https://sites.google.com/view/ot-ziv/%D0%BD%D0%B0%D0%BF%D0%B8%D1%81%D0%B0%D1%82%D1%8C-%D0%BE%D1%82%D0%B7%D1%8B%D0%B2"
+  "https://www.google.com/url?q=https://ot-ziv.com/..."
 ];
 self.addEventListener("install", (event) => {
   event.waitUntil(
